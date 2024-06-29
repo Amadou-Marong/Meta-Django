6a7cb27d299b34a533d802d754167b46bc2d9156
Amadou-Marong <marongamadou6@gmail.com>
2024-06-29 19:36:16 +0000

    response headers

--- UrlWorkspace/myproject/myapp/views.py
+++ UrlWorkspace/myproject/myapp/views.py
@@ -4,6 +4,23 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     path = request.path
-    response = HttpResponse("This Works!")
-    # return HttpResponse(path, content_type='text/html', charset='utf-8')
-    return response
+    scheme = request.scheme
+    method = request.method
+    address = request.META['REMOTE_ADDR']
+    user_agent = request.META['HTTP_USER_AGENT']
+    path_info = request.path_info
+
+    response = HttpResponse()
+    response.headers['Age'] = 20
+
+    msg = f"""<br>
+        <br>Path: {path}
+        <br>Address: {address} 
+        <br>Scheme: {scheme} 
+        <br>Method: {method} 
+        <br>User Agent: {user_agent} 
+        <br>Path Info: {path_info} 
+        <br>Response header: {response.headers} 
+
+    """
+    return HttpResponse(msg, content_type="text/html", charset="utf-8")
