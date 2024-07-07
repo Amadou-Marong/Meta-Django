9f734824fc4dfed53725d86f0c730d4766f1724b
Amadou-Marong <marongamadou6@gmail.com>
2024-07-07 08:37:45 +0000

    working with forms in django

--- WorkplaceORM/myproject/myapp/views.py
+++ WorkplaceORM/myproject/myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from myapp.forms import DemoForm
+
+def form_view(request):
+    form = DemoForm()
+    context = {"form": form}
+    return render(request, "home.html", context)
