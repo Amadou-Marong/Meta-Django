d6ba9f6e43a9b2252b75ec14458a23672d4bf671
Amadou-Marong <marongamadou6@gmail.com>
2024-07-07 10:39:32 +0000

    working with model forms

--- ModelFormWorkplace/myproject/myapp/forms.py
+++ ModelFormWorkplace/myproject/myapp/forms.py
@@ -1,1 +1,13 @@
 from django import forms
+
+SHIFTS = (
+    ('1', 'Morning'),
+    ('2', 'Afternoon'),
+    ('3', 'Evening'),
+)
+
+class LogForm(forms.Form):
+    first_name = forms.CharField(max_length=200, required=True, widget=forms.TextInput(attrs={"class": "form-control"}))
+    last_name = forms.CharField(max_length=200, required=True, widget=forms.TextInput(attrs={"class": "form-control"}))
+    shift = forms.CharField(max_length=1, widget=forms.Select(choices=SHIFTS, attrs={"class" : "form-control"}), required=True,)
+    time_log = forms.TimeField(widget=forms.TimeInput(attrs={"type": "time", "class": "form-control"}))
--- ModelFormWorkplace/myproject/myapp/views.py
+++ ModelFormWorkplace/myproject/myapp/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .forms import LogForm
 
+def form_view(request):
+    form = LogForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'home.html', context)
