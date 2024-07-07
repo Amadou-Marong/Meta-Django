565a6f746977fc4843c5dd19e71c1c43f50b8440
Amadou-Marong <marongamadou6@gmail.com>
2024-07-07 09:15:07 +0000

    working with forms in django style attribute

--- WorkplaceORM/myproject/myapp/forms.py
+++ WorkplaceORM/myproject/myapp/forms.py
@@ -10,3 +10,4 @@ FAVOURITE_DISH = [
 
 class DemoForm(forms.Form):
     reservation_date = forms.DateField(widget=NumberInput(attrs={"type": "date", "class": "form-control"}))
+    favorite_dish = forms.ChoiceField(choices=FAVOURITE_DISH, widget=forms.RadioSelect(attrs={"class": "form-control"}))
