7c41c031fa0c3fac6ecd9055ef30109f33052eab
Amadou-Marong <marongamadou6@gmail.com>
2024-07-07 09:09:16 +0000

    working with forms in django style attribute

--- WorkplaceORM/myproject/myapp/forms.py
+++ WorkplaceORM/myproject/myapp/forms.py
@@ -1,4 +1,12 @@
 from django import forms
 
+from django.forms.widgets import NumberInput
+
+FAVOURITE_DISH = [
+    ('italian', 'Italian'),
+    ('greek', 'Greek'),
+    ('turkish', 'Turkish'),
+]
+
 class DemoForm(forms.Form):
-    name = forms.CharField(widget=forms.Textarea)
+    reservation_date = forms.DateField(widget=NumberInput(attrs={"type": "date", "class": "form-control"}))
