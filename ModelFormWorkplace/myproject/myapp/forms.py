c117dc9ba1939f0cd7b7dc1429f65723a544fa9c
Amadou-Marong <marongamadou6@gmail.com>
2024-07-07 10:51:23 +0000

    register logger model

--- ModelFormWorkplace/myproject/myapp/forms.py
+++ ModelFormWorkplace/myproject/myapp/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 
-SHIFTS = (
-    ('1', 'Morning'),
-    ('2', 'Afternoon'),
-    ('3', 'Evening'),
-)
+from .models import Logger
 
-class LogForm(forms.Form):
-    first_name = forms.CharField(max_length=200, required=True, widget=forms.TextInput(attrs={"class": "form-control"}))
-    last_name = forms.CharField(max_length=200, required=True, widget=forms.TextInput(attrs={"class": "form-control"}))
-    shift = forms.CharField(max_length=1, widget=forms.Select(choices=SHIFTS, attrs={"class" : "form-control"}), required=True,)
-    time_log = forms.TimeField(widget=forms.TimeInput(attrs={"type": "time", "class": "form-control"}))
+class LogForm(forms.ModelForm):
+    class Meta:
+        model = Logger
+        fields = '__all__'
+        widgets = {
+            'first_name': forms.TextInput(attrs={"class": "form-control"}),
+            'last_name': forms.TextInput(attrs={"class": "form-control"}),
+            'shift': forms.Select(attrs={"class": "form-control"}),
+            'time_log': forms.TimeInput(attrs={"type": "time", "class": "form-control"})
+        }
--- ModelFormWorkplace/myproject/myapp/models.py
+++ ModelFormWorkplace/myproject/myapp/models.py
@@ -1,14 +1,8 @@
-# from django.db import models
+from django.db import models
 
-# # Create your models here.
-# SHIFTS = (
-#     ('1', 'Morning'),
-#     ('2', 'Afternoon'),
-#     ('3', 'Evening'),
-# )
+# Create your models here.
 
-# class LogForm(models.Model):
-#     first_name = models.CharField(max_length=200, required=True)
-#     last_name = models.CharField(max_length=200, required=True)
-#     shift = models.CharField(max_length=1, choices=SHIFTS, required=True)
-#     time_log = models.TimeField(auto_now_add=True)
+class Logger(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    time_log = models.TimeField(help_text="Enter the time")	
